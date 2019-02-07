d4515e32559c7804d995672a6552ef89c547781f
dave-yang <daworld.ny@gmail.com>
2019-02-08 01:27:03 +0500

    Initial module file with routes

---
+++ Module.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Scaffold;
+
+use Krystal\Application\Module\AbstractModule;
+
+final class Module extends AbstractModule
+{
+    /**
+     * Returns routes of this module
+     * 
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return include(__DIR__) . '/Config/routes.php';
+    }
+
+    /**
+     * Returns prepared service instances of this module
+     * 
+     * @return array
+     */
+    public function getServiceProviders()
+    {
+        return array(
+        );
+    }
+}
