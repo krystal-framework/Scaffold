d1d4a0d3b6af87315e7c059b9d723ca117fd9b5f
dave-yang <daworld.ny@gmail.com>
2019-02-09 18:31:43 +0500

    Added guessName()

--- Service/MapperGenerator.php
+++ Service/MapperGenerator.php
@@ -4,9 +4,21 @@ namespace Scaffold\Service;
 
 use Scaffold\Collection\StorageCollection;
 use Krystal\Stdlib\ArrayUtils;
+use Krystal\Text\TextUtils;
 
 final class MapperGenerator
 {
+    /**
+     * Guess mapper name from its table
+     * 
+     * @param string $table
+     * @return string
+     */
+    public static function guessName($table)
+    {
+        return TextUtils::studly($table) . 'Mapper';
+    }
+
     /**
      * Returns database engines
      * 
