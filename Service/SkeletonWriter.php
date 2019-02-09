8ae7c8fdf13d60a62778cfc21911019e3db4db2c
dave-yang <daworld.ny@gmail.com>
2019-02-09 17:01:42 +0500

    Initial SkeletonWrite service

---
+++ Service/SkeletonWriter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Scaffold\Service;
+
+use Krystal\Filesystem\FileManager;
+
+final class SkeletonWriter
+{
+    /**
+     * Module directory
+     * 
+     * @var string
+     */
+    private $moduleDir;
+
+    /**
+     * State initialization
+     * 
+     * @param string $moduleDir Module directory
+     * @return void
+     */
+    public function __construct($moduleDir)
+    {
+        $this->moduleDir = $moduleDir;
+    }
+
+    /**
+     * Writes data mapper skeleton file on the disk
+     * 
+     * @param string $module Module name
+     * @param string $engine Database engine name (MySQL, Memory, etc)
+     * @param string $mapper Mapper name
+     * @return boolean Depending on write success
+     */
+    public function saveMapper($module, $engine, $mapper, $content)
+    {
+        // Directory path
+        $dirPath = sprintf('%s/%s/Storage/%s', $this->moduleDir, $module, $engine);
+
+        // Create directory if one doesn't exist
+        FileManager::createDir($dirPath);
+
+        // Generate file path
+        $filePath = sprintf('%s/%s.php', $dirPath, $mapper);
+
+        // And do save it!
+        return file_put_contents($filePath, $content);
+    }
+}
