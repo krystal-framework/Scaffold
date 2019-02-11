7d32a427b70f093084fae081c9bb2a1efb5bcc6e
dave-yang <daworld.ny@gmail.com>
2019-02-11 11:22:39 +0500

    Move items out of view and fixed translations

--- Controller/Scaffold.php
+++ Controller/Scaffold.php
@@ -11,6 +11,34 @@ final class Scaffold extends AbstractController
      */
     public function indexAction()
     {
-        return $this->view->render('index');
+        $items = array(
+            array(
+                'name' => 'Module',
+                'description' => 'A module is a container of controllers, routes, mappers and services for particular task',
+                'route' => 'Scaffold:Module@indexAction'
+            ),
+
+            array(
+                'name' => 'Mapper',
+                'description' => 'A mapper encapsulates common operations on a single table',
+                'route' => 'Scaffold:Mapper@indexAction'
+            ),
+
+            array(
+                'name' => 'Service',
+                'description' => 'In MVC, a service is a bridge between data mapper and domain object (if any)',
+                'route' => 'Scaffold:Service@indexAction'
+            ),
+            
+            array(
+                'name' => 'Controller',
+                'description' => 'A controller responds to route matches',
+                'route' => 'Scaffold:Controller@indexAction'
+            )
+        );
+
+        return $this->view->render('index', array(
+            'items' => $items
+        ));
     }
 }
